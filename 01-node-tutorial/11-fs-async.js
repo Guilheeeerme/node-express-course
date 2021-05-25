fffc944131d90876bffa10caabdf6c4e4396a9bd
Guilheeeerme <guilherme_moura.10222@outlook.com>
2021-05-25 15:32:00 -0300

    chore: Fs Built-in Module

---
+++ 01-node-tutorial/11-fs-async.js
@@ -0,0 +1,35 @@
+// Built-in Module ( Módulo integrado )
+
+// FS - O módulo File System permite interagir com o sistema de arquivos, de forma sìncrona ou de forma assîncrona
+
+const { readFile, writeFile } = require("fs");
+
+// Callback Hell :P
+
+console.log("start");
+readFile("./content/first.txt", "utf8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./content/second.txt", "utf8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./content/result-async.txt",
+      `Here is the result : ${first}, ${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log("done with this task");
+      }
+    );
+  });
+});
+console.log("starting next task");
